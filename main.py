eaf200afb355805916a4ad8ae116d93f15ad71db
Byeongcheol Kim <kbc20000@gmail.com>
2022-07-13 20:00:47 +0900

    AutoCrawler: Added the message by each execution
    * Added the message shown by each execution.
    * Changed the obscure name of variables : factoryName(ft_name), measureDate(todayDate), saveDate(thisDate)
    * Changed the cycle to 30 minutes from 25 minutes.
    
    Signed-off-by: Byeongcheol Kim <kbc20000@gmail.com>

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-import requests, bs4
-import pandas as pd
-from pandas import DataFrame
-from lxml import html
-import numpy as np
 import datetime as dt
 import time
-import schedule
-from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus, unquote
+from urllib.request import urlopen
 
+import bs4
+import pandas as pd
+import schedule
 
 # Defualt variables
 url = "http://apis.data.go.kr/B552584/cleansys/rltmMesureResult"
 api_key = unquote('')
 todayDate = ""
 filePath = "./TMS데이터"
-ft_name = ""
+factoryName = ""
+measureDate = ""
 stackCode = ['1', '123', '132', '15', '153', '154', '155', '156', '16', '17', '18', '2', '20', '24', '25',
              '26', '27', '28', '29', '3', '30', '31', '32', '45', '47', '49', '51', '52', '53', '54', '92', '93']
 
 
 # Feature: Crawling
 def crawling():
-    global todayDate, ft_name
+    saveDate = dt.datetime.now().strftime('%H-%M-%S')
+    executeDate = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    global todayDate, factoryName, measureDate
     
     # Problem: When DataFrame is saved to .csv file, the next list was appended at previous list. 
     # Then .csv file has the bigger file size.
@@ -43,7 +43,7 @@ def crawling():
         result = urlopen(url + queryParams)
         
         # This code is for debugging
-        # print("디버깅 URL 출력\n", url + queryParams)
+        print("Exhaust connected successfully: #" + str(stackCode[i]))
         xmlObj = bs4.BeautifulSoup(result, 'lxml-xml')
 
         data = xmlObj.find_all('item')
@@ -72,26 +72,26 @@ def crawling():
                     nox_exhst_perm_stdr_value, nox_mesure_value, sox_exhst_perm_stdr_value, sox_mesure_value,
                     tsp_exhst_perm_stdr_value, tsp_mesure_value, hf_exhst_perm_stdr_value, hf_mesure_value,
                     hcl_exhst_perm_stdr_value, hcl_mesure_value, co_exhst_perm_stdr_value, co_mesure_value]
-            todayDate = mesure_dt.replace(":", "-")
-            ft_name = fact_manage_nm
+            measureDate = mesure_dt.replace(":", "-")
+            factoryName = fact_manage_nm
             dataList.append(data)
-        
-        # This code is for debugging
-        print(todayDate)
+  
+    print("\nChecking the measure date: ", measureDate)
+    print("\nExecuted: data -> df(DataFrame)")
 
     df = pd.DataFrame(dataList, columns=['측정시간', '지역명', '사업장명', '배출구', '암모니아_허용기준', '암모니아_측정값',
                                          '질소산화물_허용기준', '질소산화물_측정값', '황산화물_허용기준', '황산화물_측정값',
                                          '먼지_허용기준', '먼지_측정값', '불화수소_허용기준', '불화수소_측정값', '염화수소_허용기준',
                                          '염화수소_측정값', '일산화탄소_허용기준', '일산화탄소_측정값'])
-    
-    # This code is for debugging
-    print(df)
-    
-    
-    # Added crawling time to distinguish whether crawled data is the same data according to server status
-    thisDate = dt.datetime.now().strftime('%H-%M-%S')
-    filename = str(ft_name) + " " + str(todayDate) + " " + thisDate
-    df.to_csv(filename + ".csv",
+
+    print("Completed: data -> df(DataFrame)")
+    print("\nChecking: The length of df row/col: " + str(df.shape[0]) + " rows/" + str(df.shape[1]) + " cols")
+    print("Checking: The contents of df: \n", df)
+
+    fileName = str(factoryName) + " " + str(measureDate) + " " + saveDate
+
+    print("\nExecuted: df(DataFrame) -> .csv")
+    df.to_csv(fileName + ".csv",
               sep=',',
               na_rep='NaN',
               float_format='%.2f',
@@ -100,10 +100,16 @@ def crawling():
               line_terminator="\n",
               encoding='utf-8-sig',
               mode='w')
+    
+    print("Completed: df(DataFrame) -> " + fileName + ".csv")
+    print("Completed: Saved successfully", str(saveDate.replace("-", ":")))
 
 
-# Crawled every 25 minutes
-schedule.every(25).minutes.do(crawling)
+print("AutoCrawler is starting...")
+
+
+# Crawled every 30 minutes
+schedule.every(30).minutes.do(crawling, "\nAutoCrawler is running...")
 
 while True:
     schedule.run_pending()
